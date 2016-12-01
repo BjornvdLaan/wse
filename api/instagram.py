b6f9b6de7ed20fa50600159e232bfbce5851b7e0
Bjorn van der Laan <bjorn.vdlaan@gmail.com>
2016-12-01 17:02:34 +0100

    cleaned repo, added support for user/{user_id} endpoint

--- api/instagram.py
+++ api/instagram.py
@@ -3,13 +3,16 @@ import requests
 
 class Instagram:
     def __init__(self, access_token):
-        self.access_token = access_token
+        self.params = {
+            "access_token": access_token
+        }
 
     def get_users_self(self):
-        params = {
-            "access_token": self.access_token
-        }
-        r = requests.get('https://api.instagram.com/v1/users/self/', params=params)
+        r = requests.get('https://api.instagram.com/v1/users/self/', params=self.params)
+        return r.json()
+
+    def get_users(self, user_id):
+        r = requests.get('https://api.instagram.com/v1/users/' + user_id + '/', params=self.params)
         return r.json()
 
 
